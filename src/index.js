59baa382f711339cffefcfb210b920d97c970a42
riad_zakir@yahoo.com <riad_zakir@yahoo.com>
2021-10-04 13:56:31 +0600

    User to global state and loader

--- src/index.js
+++ src/index.js
@@ -5,21 +5,53 @@ import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase"
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { setUser } from "./actions";
+import rootReducer from "./reducers";
+import Spinner from "./Spinner";
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
-);
+const store = createStore(rootReducer, composeWithDevTools())
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push('/');
+      }
+    })
+  }
+
+  render() {
+    return this.props.isLoading ? <Spinner /> : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStatesToProps = (state) => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(connect(mapStatesToProps, { setUser })(Root));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>
+  , document.getElementById("root"));
 registerServiceWorker();
